9c474642b2225e4bbf930954edf1f4c07afebb44
Abigail McFarlane <abigail.mcfarlane@mymona.uwi.edu>
2022-10-09 16:20:13 -0500

    Exercise 1 tic-tac-toe.js

---
+++ tic-tac-toe.js
@@ -0,0 +1,25 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", ()=>{
+    
+    const squares = document.querySelectorAll("#board > div");
+    
+    squares.forEach((square)=>{
+        square.classList.add("square");
+        square.innerHTML ="";
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
